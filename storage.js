82a8c60fd634f68c2fadac626bbe9bbe466b5bab
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-07-02 21:13:28 +0200

    refactor(local-storage): extract common storage functions

--- storage.js
+++ storage.js
@@ -1,22 +1,29 @@
 // Local storage
-
-const STORAGE_KEY_TRANSACTIONS = 'pocket-budget-transactions';
+const STORAGE_KEY = {
+  TRANSACTIONS: 'pocket-budget-transactions',
+};
 
 export function loadTransactions() {
-  const data = localStorage.getItem(STORAGE_KEY_TRANSACTIONS);
-
-  if (!data) return [];
-
-  try {
-    const parsed = JSON.parse(data);
-    if (!Array.isArray(parsed)) return [];
-    return parsed;
-  } catch {
-    console.error('Failed to parse transactions');
-    return [];
-  }
+  return loadFromLS(STORAGE_KEY.TRANSACTIONS, []);
 }
 
 export function saveTransactions(transactions) {
-  localStorage.setItem(STORAGE_KEY_TRANSACTIONS, JSON.stringify(transactions));
+  saveToLS(STORAGE_KEY.TRANSACTIONS, transactions);
+}
+
+function loadFromLS(key, defaultValue) {
+  const data = localStorage.getItem(key);
+
+  if (!data) return defaultValue;
+
+  try {
+    return JSON.parse(data);
+  } catch {
+    console.error(`Failed to parse localStorage: ${key}`);
+    return defaultValue;
+  }
+}
+
+function saveToLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
 }
